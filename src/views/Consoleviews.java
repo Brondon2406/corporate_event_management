433307201b715e9fa6ac3fd3ae524bc9c862a01f
Severin_k <severinkengne2001@gmail.com>
2025-08-19 12:22:41 +0100

    appel des methodes registerUser et loginUser

--- src/views/Consoleviews.java
+++ src/views/Consoleviews.java
@@ -1,15 +1,13 @@
 package views;
 
 import java.util.Scanner;
-
-import controller.dto.Userdto;
-import model.entity.Users;
+import model.service.AuthenticationService;
+import model.service.implementation.AuthenticationServiceImpl;
 
 public class Consoleviews {
-	
-	  
-
-	    	    public static void main(String[] args) {
+		 	    	
+				public static void main(String[] args) {
+					AuthenticationService authentication = new AuthenticationServiceImpl (); 
 	    	        Scanner scanner = new Scanner(System.in);
 	    	        boolean continuer = true;
 
@@ -22,29 +20,27 @@ public class Consoleviews {
 	    	            System.out.println("3 - Quitter");
 	    	            System.out.print("Choisissez une option : ");
 
-	    	            int choix = scanner.nextInt();
-	    	            scanner.nextInt();
-	    	            
-	    	            switch (choix) {
-	    	                case 1:
-	    	                	Userdto.registerUser();
-	    	                    break;
-	    	                case 2:
-	    	                	Userdto.loginUser();
-	    	                    break;
-	    	                case 3:
-	    	                    System.out.println("Au revoir !");
-	    	                    continuer = false;
-	    	                    break;
-	    	                default:
-	    	                    System.out.println("Choix invalide, veuillez réessayer.");
-	    	                    break;
+	    	            int choix = scanner.nextInt();	    	            	    	            
+	    	         
+						switch (choix) {
+	    	             case 1: 
+	    	            	 authentication.registerUser();
+	    	            	break;
+	    	             case 2:
+	    	            	 authentication.loginUser();
+	    	                break;
+	    	             case 3:
+	    	                 System.out.println("Au revoir !");
+	    	                 continuer = false;
+	    	                 break;
+	    	              default:
+	    	                  System.out.println("Choix invalide, veuillez réessayer.");
+	    	                  break;
 	    	            }
-	    	        }
+	    	        } 
 
 	    	        scanner.close();
-	    	    }
-
+	    	   }
 	    	   
 
 }
