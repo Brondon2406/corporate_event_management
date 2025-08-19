924d202f7293e9e7253c51b340edf55b7fd4a751
Severin_k <severinkengne2001@gmail.com>
2025-08-19 12:33:48 +0100

    modification du type de l'attribut role

--- src/controller/dto/Userdto.java
+++ src/controller/dto/Userdto.java
@@ -1,15 +1,17 @@
 package controller.dto;
 
+import model.entity.enumeration.Role;
+
 public class Userdto {
 	
 	private int id;
 	private String name;
 	private String email ;
 	private String password ;
-	private String role;
+	private Role role;
 	private String fonction;
 	
-	public Userdto(int id, String name, String email, String password, String role, String fonction) {
+	public Userdto(int id, String name, String email, String password, Role role, String fonction) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -18,9 +20,10 @@ public class Userdto {
 		this.fonction = fonction;
 	}
 	
-	public Userdto(String name, String email, String role, String fonction) {
+	public Userdto(String name, String email, String password, Role role, String fonction) {
 		this.name = name;
 		this.email = email;
+		this.password = password;
 		this.role = role;
 		this.fonction = fonction;
 	}
@@ -60,11 +63,11 @@ public class Userdto {
 		this.password = password;
 	}
 
-	public String getRole() {
+	public Role getRole() {
 		return role;
 	}
 
-	public void setRole(String role) {
+	public void setRole(Role role) {
 		this.role = role;
 	}
 
@@ -84,14 +87,23 @@ public class Userdto {
 
 	
 
-	public static void loginUser() {
+	public static boolean loginUser(String email, String password) {
+		return false;
+		
+		
+	}
+
+	public static boolean registerUser(String nom, String email, String password, Role role , String fonction) {
+		return false;
+		
+	}
+
+	public void setRole(Object role) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public static void registerUser() {
-		// TODO Auto-generated method stub
-		
-	}
+	
+
 	
 }
