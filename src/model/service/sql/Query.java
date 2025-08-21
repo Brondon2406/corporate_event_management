faead8fc457871cc0f7ace05cf31abc72e997cd7
Severin_k <severinkengne2001@gmail.com>
2025-08-21 10:47:27 +0100

    modification du nom de la table et ses parametres

--- src/model/service/sql/Query.java
+++ src/model/service/sql/Query.java
@@ -2,7 +2,7 @@ package model.service.sql;
 
 public class Query {
 	
-	public static final String CREATE_USER = "INSERT INTO corporate_event_management_systeme (nom, email, password, role, fonction) VALUES (?,?,?,?,?)";
+	public static final String CREATE_USER = "INSERT INTO users (name, email, password, role, fonction) VALUES (?,?,?,?,?)";
 
 	public static final String GET_USER ="SELECT id, email, password FROM corporate_event_management_system WHERE email = ? AND password = ?";
 			
