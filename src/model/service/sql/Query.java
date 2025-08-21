1e5a1949c26cc03ddab820939b1b0711994944c1
Severin_k <severinkengne2001@gmail.com>
2025-08-21 07:47:26 +0100

    ajout des parametres

--- src/model/service/sql/Query.java
+++ src/model/service/sql/Query.java
@@ -2,6 +2,6 @@ package model.service.sql;
 
 public class Query {
 	
-	public static final String CREATE_USER = "INSERT INTO corporate_event_management_systeme (name,email,passeword,role,fonction) VALUES (?,?,?,?,?)";
-  
+	public static final String CREATE_USER = "INSERT INTO corporate_event_management_systeme () VALUES (?,?,?,?,?)";
+
 }
