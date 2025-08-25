8637d9529f17c9bfd6e24a63e74830417f21903e
Severin_k <severinkengne2001@gmail.com>
2025-08-25 12:47:38 +0100

    Implementation of user existence verification in the DB

--- src/model/service/AuthenticationService.java
+++ src/model/service/AuthenticationService.java
@@ -6,8 +6,7 @@ import model.entity.Users;
 public interface AuthenticationService {
 	
 	public Userdto registerUser (Users user);
-	public Userdto loginUser (Users user) ;
+	 public Userdto loginUser(String email, String password) ;
+		
 	
-	
-
 }
