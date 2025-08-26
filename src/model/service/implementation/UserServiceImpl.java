b9758bfd72472101ea486313ae3956b26e3098e2
Severin_k <severinkengne2001@gmail.com>
2025-08-26 18:33:40 +0100

    Method for modifying a user's profile

--- src/model/service/implementation/UserServiceImpl.java
+++ src/model/service/implementation/UserServiceImpl.java
@@ -1,29 +1,42 @@
 package model.service.implementation;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import model.dto.Userdto;
-import model.mapping.MappingService.MappingUser;
 import model.database.DatabaseConnection;
 import model.entity.Users;
 import model.service.UserService;
+import model.service.sql.Query;
 
 public class UserServiceImpl implements UserService{
-	private static final Logger LOG = LogManager.getLogger(UserServiceImpl.class);
-	private final DatabaseConnection db;
-	private final MappingUser mapper;
 	
-	public UserServiceImpl(DatabaseConnection db,  MappingUser mapper) {
-		this.db = db;
-		this.mapper = mapper;	
-	}
+	private static final Logger LOG = LogManager.getLogger(UserServiceImpl.class);
+	Connection connection = DatabaseConnection.getInstance();  
 	
 	@Override
-	public Userdto updateUser(Userdto userDTO) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public boolean updateUser(Userdto userDTO) {
+		String query = Query.UPDATE_USER ;
+		 try (PreparedStatement ps = connection.prepareStatement(query)) {
+			 
+			 	ps.setString(1, userDTO.getName());
+		        ps.setString(1, userDTO.getEmail());
+		        ps.setString(2, userDTO.getPassword());
+		        ps.setInt(3, userDTO.getId());
+
+		        int rows = ps.executeUpdate();
+		        return rows > 0;
+
+		    } catch (SQLException e) {
+		        LOG.error("Erreur updateUser: {}", e.getMessage());
+		        return false;
+		    }
+		}
 
 	@Override
 	public String deleteUser(int userId) {
