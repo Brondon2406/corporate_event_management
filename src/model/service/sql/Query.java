f015e10a462d200062699c573663ab3efcbb9d34
Severin_k <severinkengne2001@gmail.com>
2025-08-27 11:34:23 +0100

    Implementing the event data insertion query

--- src/model/service/sql/Query.java
+++ src/model/service/sql/Query.java
@@ -7,4 +7,6 @@ public class Query {
 	public static final String GET_USER ="SELECT * FROM users WHERE email = ? AND password = ?";
 
 	public static final String UPDATE_USER = "UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?";
+	
+	public static final String CREATE_EVENT = "INSERT INTO events (title, date_debut, date_fin, type_event, room_id) VALUES (?, ?, ?, ?, ?)";
 }
