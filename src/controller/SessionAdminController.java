d407bccc9343a21a3629610372405b5dab91673a
Severin_k <severinkengne2001@gmail.com>
2025-08-27 03:43:08 +0100

    This class and method allows controller to modify a user

---
+++ src/controller/SessionAdminController.java
@@ -0,0 +1,31 @@
+package controller;
+
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import model.dto.Userdto;
+import model.service.UserService;
+import model.service.implementation.UserServiceImpl;
+import util.constants.Constants;
+
+
+
+public class SessionAdminController {	
+	private static final Logger LOG = LogManager.getLogger(SessionAdminController.class);
+	private UserService userService = new UserServiceImpl();
+		
+	public void updateUser(Userdto userDTO, String newName, String newEmail, String newPassword) {
+		userDTO.setName(newName);
+		userDTO.setEmail(newEmail);
+		userDTO.setPassword(newPassword);
+
+		boolean success = userService.updateUser(userDTO);
+	    if(success) {
+	       LOG.info("Profil mis à jour avec succès !");
+	    } else {
+	    	LOG.error(Constants.ERROR_UPDATE_USER	);
+	    }
+	}
+}
