4257dfd80e8351d80f3a71914c890e9217dec645
Severin_k <severinkengne2001@gmail.com>
2025-08-29 17:05:48 +0100

    Implementation of the user control CRUD in the controller

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -4,6 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import model.dto.Userdto;
+import model.entity.Users;
+import model.mapping.MappingService.MappingUser;
+import model.mapping.MappingServiceImpl.MappingUserImpl;
 import model.service.UserService;
 import model.service.implementation.UserServiceImpl;
 import util.constants.Constants;
@@ -11,8 +14,28 @@ import util.constants.Constants;
 public class UserController {
 	private static final Logger LOG = LogManager.getLogger(UserController.class);
 	private UserService userService = new UserServiceImpl();
+	private MappingUser mapper = new MappingUserImpl();
 
-	public void updateUser(Userdto userDTO, String newName, String newEmail, String newPassword) {
+	public boolean userCreateController(Userdto userdto) {
+		Users user = mapper.convertUserdtoToUsers(userdto);
+
+		if (userdto == null) {
+			LOG.error(Constants.EMPTY_USER_DTO);
+			return false;
+		}
+
+		Userdto dto = userService.createUser(user);
+
+		if (dto != null) {
+			LOG.info("Événement créé avec succès !");
+			return true;
+		} else {
+			LOG.error(Constants.ERROR_DURING_USER_INSERTION);
+			return false;
+		}
+	}
+
+	public void updateUserController(Userdto userDTO, String newName, String newEmail, String newPassword) {
 		userDTO.setName(newName);
 		userDTO.setEmail(newEmail);
 		userDTO.setPassword(newPassword);
@@ -24,4 +47,13 @@ public class UserController {
 			LOG.error(Constants.ERROR_UPDATE_USER);
 		}
 	}
+
+	public boolean userDeleteController(int eventId) {
+		return userService.deleteUser(eventId);
+	}
+
+	public Userdto getEventByIdController(int id) {
+		return userService.getUserById(id);
+	}
+
 }
