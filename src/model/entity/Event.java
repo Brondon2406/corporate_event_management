ac4537eeea9ef7e1c64db0bdb4ecf91f93f20b5f
Severin_k <severinkengne2001@gmail.com>
2025-09-02 08:41:26 +0100

    adding attributes to the event entity

--- src/model/entity/Event.java
+++ src/model/entity/Event.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import model.entity.enumeration.TypeEvent;
+import model.dto.Userdto;
+import model.entity.enumeration.Format;
 
 public class Event {
 
@@ -13,37 +15,55 @@ public class Event {
 	private LocalDateTime dateFin;
 	private TypeEvent typeEvent;
 	private EventRoom eventRoom;
-	private List<Users> users;
+	private Format format;
+	private String moderator;
+	private String tutor;
+	private List<Userdto> users;
+	private List<String> externalParticipantsEmails;
 
 	public Event(int id, String title, LocalDateTime dateDebut, LocalDateTime dateFin, TypeEvent typeEvent,
-			EventRoom eventRoom) {
+			EventRoom eventRoom, Format format,List<String> externalParticipantsEmails, String moderator, String tutor, List<Userdto> users) {
 		this.id = id;
 		this.title = title;
 		this.dateDebut = dateDebut;
 		this.dateFin = dateFin;
 		this.typeEvent = typeEvent;
 		this.eventRoom = eventRoom;
+		this.format = format;
+		this.moderator = moderator;
+		this.tutor = tutor;
+		this.users = users;
+		this.externalParticipantsEmails = externalParticipantsEmails;
 	}
 
 	public Event(String title, LocalDateTime dateDebut, LocalDateTime dateFin, TypeEvent typeEvent,
-			EventRoom eventRoom) {
+			EventRoom eventRoom, Format format, String moderator, String tutor, List<Userdto> users, List<String> externalParticipantsEmails) {
 		this.title = title;
 		this.dateDebut = dateDebut;
 		this.dateFin = dateFin;
 		this.typeEvent = typeEvent;
 		this.eventRoom = eventRoom;
+		this.format = format;
+		this.moderator = moderator;
+		this.tutor = tutor;
+		this.users = users;
+		this.externalParticipantsEmails = externalParticipantsEmails;
 	}
 
+	
 	public Event(String title, LocalDateTime dateDebut, LocalDateTime dateFin, TypeEvent typeEvent, EventRoom eventRoom,
-			List<Users> users) {
+			List<Userdto> users, Format format, String moderator, String tutor) {
 		super();
 		this.title = title;
 		this.dateDebut = dateDebut;
 		this.dateFin = dateFin;
 		this.typeEvent = typeEvent;
 		this.eventRoom = eventRoom;
+		this.format = format;
 		this.users = users;
+
 	}
+
 
 	public Event() {
 	}
@@ -96,11 +116,45 @@ public class Event {
 		this.eventRoom = eventRoom;
 	}
 
-	public List<Users> getUsers() {
+	public List<Userdto> getUsers() {
 		return users;
 	}
 
-	public void setUsers(List<Users> users) {
+	public void setUsers(List<Userdto> users) {
 		this.users = users;
 	}
+
+	public Format getFormat() {
+		return format;
+	}
+
+	public void setFormat(Format format) {
+		this.format = format;
+	}
+	
+	public String getModerator() {
+		return moderator;
+	}
+
+	public void setModerator(String moderator) {
+		this.moderator = moderator;
+	}
+
+	public String getTutor() {
+		return tutor;
+	}
+
+	public void setTutor(String tutor) {
+		this.tutor = tutor;
+	}
+	
+	public List<String> getExternalParticipantsEmails() {
+		return externalParticipantsEmails;
+	}
+
+	public void setExternalParticipantsEmails(List<String> externalParticipantsEmails) {
+		this.externalParticipantsEmails = externalParticipantsEmails;
+	}
+
+
 }
