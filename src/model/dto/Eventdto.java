18b56a6749255ddf576d71a55ee89d067228b76a
Severin_k <severinkengne2001@gmail.com>
2025-09-02 08:46:04 +0100

    adding attributes to the eventdto entity

--- src/model/dto/Eventdto.java
+++ src/model/dto/Eventdto.java
@@ -1,7 +1,7 @@
 package model.dto;
 
-
 import java.time.LocalDateTime;
+
 import java.util.List;
 
 import model.entity.EventRoom;
@@ -14,20 +14,29 @@ public class Eventdto {
 	private LocalDateTime dateFin;
 	private String typeEvent;
 	private EventRoom eventRoom;
+	private String format;
+	private String moderator;
+	private String tutor;
 	private List<Userdto> users;
+	private List<String> externalParticipantsEmails;
 
 	public Eventdto(int id, String title, LocalDateTime dateDebut, LocalDateTime dateFin, String typeEvent,
-			EventRoom eventRoom) {
+			EventRoom eventRoom, String format, String moderator, String tutor, List<String> externalParticipantsEmails, List<Userdto> users) {
 		this.id = id;
 		this.title = title;
 		this.dateDebut = dateDebut;
 		this.dateFin = dateFin;
 		this.typeEvent = typeEvent;
 		this.eventRoom = eventRoom;
+		this.format = format;
+		this.moderator = moderator;
+		this.tutor = tutor;
+		this.users = users;	
+		this.externalParticipantsEmails = externalParticipantsEmails;
 	}
 
 	public Eventdto(String title, LocalDateTime dateDebut, LocalDateTime dateFin, String typeEvent, EventRoom eventRoom,
-			List<Userdto> users) {
+			List<Userdto> users, String moderator, String tutor) {
 		super();
 		this.title = title;
 		this.dateDebut = dateDebut;
@@ -35,11 +44,26 @@ public class Eventdto {
 		this.typeEvent = typeEvent;
 		this.eventRoom = eventRoom;
 		this.users = users;
+		this.format = moderator;
+		this.tutor = tutor;
+		
+
 	}
 
-	public Eventdto() {
+	
+	public Eventdto(int id, String string, LocalDateTime object, LocalDateTime object2, String string2,
+			String string3, String string4, String string5,EventRoom room) {
+		// TODO Auto-generated constructor stub
 	}
 
+
+	public Eventdto(String title2, LocalDateTime dateDebut2, LocalDateTime dateFin2, String typeEvent2,
+			EventRoom eventroom2, List<Userdto> users2, Userdto moderator2, Userdto tutor2) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Eventdto() {};
 
 	public int getId() {
 		return id;
@@ -96,4 +120,37 @@ public class Eventdto {
 	public void setUsers(List<Userdto> users) {
 		this.users = users;
 	}
+
+	public List<String> getExternalParticipantsEmails() {
+		return externalParticipantsEmails;
+	}
+
+	public void setExternalParticipantsEmails(List<String> externalParticipantsEmails) {
+		this.externalParticipantsEmails = externalParticipantsEmails;
+	}
+
+	public String getModerator() {
+		return moderator;
+	}
+
+	public void setModerator(String moderator) {
+		this.moderator = moderator;
+	}
+
+	public String getTutor() {
+		return tutor;
+	}
+
+	public void setTutor(String tutor) {
+		this.tutor = tutor;
+	}
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
 }
