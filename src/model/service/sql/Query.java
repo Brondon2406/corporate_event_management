139f7c8382e7b1d8f9dbf3210ad8f6e1d3c35f33
Severin_k <severinkengne2001@gmail.com>
2025-09-11 17:29:23 +0100

    request to add and remove internal and external participants

--- src/model/service/sql/Query.java
+++ src/model/service/sql/Query.java
@@ -8,35 +8,35 @@ public class Query {
 
 	public static final String UPDATE_USER = "UPDATE users SET name = ?,first_name = ?, email = ?, password = ? WHERE id = ?";
 
-	public static final String CREATE_EVENT = "INSERT INTO events (title, date_debut, date_fin, type_event, format_event, moderator, tutor, id_planning, room_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	public static final String CREATE_EVENT = "INSERT INTO events (title, date_debut, date_fin, type_event, format_event, moderator, tutor, id_planning, room_id, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-	public static final String UPDATE_EVENT = "UPDATE events SET title = ?, date_debut = ?, date_fin = ?, type_event = ?, room_id = ? WHERE id = ?";
+	public static final String UPDATE_EVENT = "UPDATE events SET title = ?, date_debut = ?, date_fin = ?, type_event = ?, format_event = ?, moderator = ?, room_id = ?, moderator = ? WHERE id = ?";
 
 	public static final String DELETE_EVENT = "DELETE FROM events WHERE id = ?";
 
-	public static final String SELECT_EVENT_BY_ID_AND_NAME = "SELECT * FROM events_room WHERE id = ? AND name = ?";
+	public static final String GET_EVENT_BY_ID_AND_NAME = "SELECT * FROM events_room WHERE id = ? AND name = ?";
 
-	public static final String SELECT_EVENT_BY_ID = "SELECT * FROM events WHERE id = ?";
+	public static final String GET_EVENT_BY_ID = "SELECT * FROM events WHERE id = ?";
 
-	public static final String SELECT_USER_BY_ID = "SELECT id, name,first_name, email, role, fonction FROM users WHERE id = ?";
+	public static final String GET_USER_BY_ID = "SELECT id, name,first_name, email, role, fonction FROM users WHERE id = ?";
 
 	public static final String DELETE_USER = "DELETE FROM users WHERE id = ?";
 
-	public static final String GET_ALL_EVENTS = "SELECT title, date_debut, date_fin, type_event, format_event, moderator, tutor, id_planning, room_id FROM events";
+	public static final String GET_ALL_EVENTS = "SELECT * FROM events";
 
-	public static final String SELECT_EVENTROOM_BY_ID_AND_NAME = "SELECT * FROM event_rooms WHERE id = ? AND name = ?";
+	public static final String GET_EVENTROOM_BY_ID_AND_NAME = "SELECT * FROM event_rooms WHERE id = ? AND name = ?";
 
-	public static final String SELECT_ALL_ACTIVE_ROOMS = "SELECT * FROM event_rooms WHERE active = TRUE";
+	public static final String GET_ALL_ACTIVE_ROOMS = "SELECT * FROM event_rooms WHERE active = TRUE";
 
-	public static final String SELECT_EVENTROOM_BY_ID = "SELECT * FROM event_rooms WHERE id = ? AND active = true";
+	public static final String GET_EVENTROOM_BY_ID = "SELECT * FROM event_rooms WHERE id = ? AND active = true";
 
-	public static final String GET_ALL_USERS = "SELECT id, first_name, name, email, role, fonction FROM users";
+	public static final String GET_ALL_USERS = "SELECT * FROM users";
 
-	public static final String INSERT_EVENT_USER = "INSERT INTO event_users (event_id, user_id) VALUES (?, ?)";
+	public static final String CREATE_EVENT_USER = "INSERT INTO event_users (event_id, user_id) VALUES (?, ?)";
 
 	public static final String CREATE_PLANNING = "INSERT INTO planning (motif, date_debut, date_fin,tutor_planning) VALUES (?,?,?,?) ";
 
-	public static final String SELECT_PLANNING_BY_ID = "SELECT * FROM planning WHERE id = ?";;
+	public static final String GET_PLANNING_BY_ID = "SELECT * FROM planning WHERE id = ?";;
 
 	public static final String DELETE_PLANNING = "DELETE FROM planning WHERE id = ?";
 
@@ -48,16 +48,28 @@ public class Query {
 
 	public static final String DELETE_EVENTROOM = "DELETE FROM event_rooms WHERE id = ?";
 
-	public static final String SELECT_ALL_PLANNING = "SELECT * FROM planning";
+	public static final String GET_ALL_PLANNING = "SELECT * FROM planning";
 
-	public static final String SELECT_PLANNINGS_BY_PERIOD = "SELECT p.id, p.motif, p.date_debut, p.date_fin, u.id AS tutor_id, u.name AS tutor_name, u.first_name AS tutor_first_name FROM planning p LEFT JOIN users u ON p.tutor_planning = u.id WHERE p.date_debut >= ? AND p.date_fin <= ?";
+	public static final String GET_PLANNINGS_BY_PERIOD = "SELECT p.id, p.motif, p.date_debut, p.date_fin, u.id AS tutor_id, u.name AS tutor_name, u.first_name AS tutor_first_name FROM planning p LEFT JOIN users u ON p.tutor_planning = u.id WHERE p.date_debut >= ? AND p.date_fin <= ?";
 
-		public static final String SELECT_ALL_EVENTROOMS = "SELECT * FROM event_rooms";
+	public static final String GET_ALL_EVENTROOMS = "SELECT * FROM event_rooms";
 
-	public static final String SELECT_EVENTROOMS_BY_ID = "SELECT * FROM event_rooms WHERE id = ?";
+	public static final String GET_EVENTROOMS_BY_ID = "SELECT * FROM event_rooms WHERE id = ?";
 
-	public static final String SELECT_ALL_ROOMS = "SELECT * FROM event_rooms";
+	public static final String GET_USERS_BY_ROLE = "SELECT * FROM users WHERE Role = ?";
 
 	public static final String DELETE_EVENTS_IN_PLANNING = "DELETE FROM events WHERE id_planning = ?";
+
+	public static final String LINK_EVENT_WITH_INTERNAL_USERS =  "INSERT IGNORE INTO event_users (event_id, user_id) VALUES (?, ?)";
+
+	public static final String LINK_EVENT_WITH_EXTERNAL_USERS = "INSERT IGNORE INTO event_users (event_id, external_email) VALUES (?, ?)";
+
+	public static final String ADD_INTERNAL_USERS = "INSERT INTO event_users (event_id, user_id) VALUES (?, ?)";
+
+	public static final String REMOVE_INTERNAL_USERS = "DELETE FROM event_users WHERE event_id = ? AND user_id = ?";
+
+	public static final String ADD_EXTERNAL_USERS = "INSERT INTO event_users (event_id, external_email) VALUES (?, ?)";
+
+	public static final String REMOVE_EXTERNAL_USERS = "DELETE FROM event_users WHERE event_id = ? AND external_email = ?";
 
 }
