7289f4541341f4c6a8238df586b8b9ddd3cc3b8a
Severin_k <severinkengne2001@gmail.com>
2025-09-15 20:57:44 +0400

    Creat Method to CRUD Event at Users

--- src/model/service/sql/Query.java
+++ src/model/service/sql/Query.java
@@ -6,7 +6,7 @@ public class Query {
 
 	public static final String GET_USER = "SELECT * FROM users WHERE email = ? AND password = ?";
 
-	public static final String UPDATE_USER = "UPDATE users SET name = ?,first_name = ?, email = ?, password = ? WHERE id = ?";
+	public static final String UPDATE_USER = "UPDATE users SET name = ?,first_name = ?, email = ?, password = ?, fonction = ? WHERE id = ?";
 
 	public static final String CREATE_EVENT = "INSERT INTO events (title, date_debut, date_fin, type_event, format_event, moderator, tutor, id_planning, room_id, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
@@ -21,6 +21,8 @@ public class Query {
 	public static final String GET_USER_BY_ID = "SELECT id, name,first_name, email, role, fonction FROM users WHERE id = ?";
 
 	public static final String DELETE_USER = "DELETE FROM users WHERE id = ?";
+
+	public static final String DELETE_USER_FROM_EVENTUSERS ="DELETE FROM event_users WHERE user_id = ?";
 
 	public static final String GET_ALL_EVENTS = "SELECT * FROM events";
 
@@ -60,9 +62,9 @@ public class Query {
 
 	public static final String DELETE_EVENTS_IN_PLANNING = "DELETE FROM events WHERE id_planning = ?";
 
-	public static final String LINK_EVENT_WITH_INTERNAL_USERS =  "INSERT IGNORE INTO event_users (event_id, user_id) VALUES (?, ?)";
+	public static final String LINK_EVENT_WITH_INTERNAL_USERS = "INSERT INTO event_users (event_id, user_id, status_user) VALUES (?, ?, 'PENDING')";
 
-	public static final String LINK_EVENT_WITH_EXTERNAL_USERS = "INSERT IGNORE INTO event_users (event_id, external_email) VALUES (?, ?)";
+	public static final String LINK_EVENT_WITH_EXTERNAL_USERS = "INSERT INTO event_users (event_id, external_email) VALUES (?, ?)";
 
 	public static final String ADD_INTERNAL_USERS = "INSERT INTO event_users (event_id, user_id) VALUES (?, ?)";
 
@@ -71,5 +73,30 @@ public class Query {
 	public static final String ADD_EXTERNAL_USERS = "INSERT INTO event_users (event_id, external_email) VALUES (?, ?)";
 
 	public static final String REMOVE_EXTERNAL_USERS = "DELETE FROM event_users WHERE event_id = ? AND external_email = ?";
+
+	public static final String GET_ALL_EMAIL_TO_USER_INTERNEL = "SELECT u.email FROM users u "
+			+ "JOIN event_users eu ON u.id = eu.user_id " + "WHERE eu.event_id = ?";
+
+	public static final String GET_EVENTS_BY_STATUS = "SELECT * FROM events WHERE status = ?";
+
+	public static final String UPDATE_EVENTS_STATUS = "UPDATE events SET status = ? WHERE id = ?";
+
+	public static final String GET_ALL_EMAIL_TO_USER_EXTERNEL = "SELECT external_email FROM event_users WHERE event_id = ?";
+
+	public static final String GET_ASSIGNED_EVENT = "SELECT e.id, e.title, e.date_debut, e.date_fin, e.format_event, "
+			+ "e.room_id, e.status, e.moderator, e.tutor, e.id_planning " + "FROM events e "
+			+ "JOIN event_users eu ON e.id = eu.event_id " + "WHERE eu.user_id = ?";
+
+	public static final String GET_USERSTATUS_FOR_EVENT = "SELECT status_user FROM event_users WHERE user_id = ? AND event_id = ?";
+
+	public static final String UPDATE_USERSTATUS_FOR_EVENT = "UPDATE event_users SET status_user = ? WHERE user_id = ? AND event_id = ?";
+
+	public static final String GET_INTERNALUSERS_FOR_EVENT = "SELECT user_id FROM event_users WHERE event_id = ?";
+
+	public static final String GET_EXTERNALUSERS_FOR_EVENT = "SELECT external_email FROM event_users WHERE event_id = ?";
+
+	public static final String UPDATE_EXPIRED_EVENTS = "UPDATE events SET status = 'EXPIRED' "
+			+ "WHERE date_fin < NOW() AND status <> 'EXPIRED'";
+
 
 }
