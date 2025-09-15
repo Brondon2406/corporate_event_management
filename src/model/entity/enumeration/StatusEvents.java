bc2a103acd236541e2e1410d6de5fc560098c1cb
Severin_k <severinkengne2001@gmail.com>
2025-09-15 20:55:47 +0400

    Restructuring of enum StatusEvent

--- src/model/entity/enumeration/StatusEvents.java
+++ src/model/entity/enumeration/StatusEvents.java
@@ -6,11 +6,11 @@ import java.util.stream.Collectors;
 
 public enum StatusEvents {
 
-	PENDING, VALIDATED, REJECTED, CANCELED, EXPIRED;
+	PENDING, VALIDATED, REJECTED, EXPIRED;
 
 	public static List<String> getAllStatus() {
-		List<String> status = Arrays.asList(TypeEvent.values()).stream().map(e -> e.name())
-				.collect(Collectors.toList());
+		List<String> status = Arrays.asList(StatusEvents.values()).stream().filter(e -> !e.equals(EXPIRED))
+				.map(e -> e.name()).collect(Collectors.toList());
 		return status;
 	}
 
