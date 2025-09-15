3087e9acfa8cea7e3221aa3ecc772144d3a25ca7
Severin_k <severinkengne2001@gmail.com>
2025-09-15 20:59:42 +0400

    Create an enum that will allow the user to confirm their participation in the event

---
+++ src/model/entity/enumeration/ParticipationUserToEvent.java
@@ -0,0 +1,26 @@
+package model.entity.enumeration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum ParticipationUserToEvent {
+
+
+	PENDING, ACCEPTED, REFUSED;
+
+	public static List<String> getAllStatus() {
+		List<String> status = Arrays.asList(TypeEvent.values()).stream().map(e -> e.name())
+				.collect(Collectors.toList());
+		return status;
+	}
+
+	public static StatusEvents fromString(String status) {
+		return status.isEmpty() ? null : StatusEvents.valueOf(status);
+	}
+
+	public boolean isEmpty() {
+		return false;
+	}
+
+}
